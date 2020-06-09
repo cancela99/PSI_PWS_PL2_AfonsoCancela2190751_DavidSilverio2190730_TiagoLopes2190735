cd34d9224dace61c9fad40795fbd01a5093fbbc2
Tiago <2190735@my.ipleiria.pt>
2020-06-09 23:36:39 +0100

    Solução para alterar os dados do utilizador, implementação do backoffice

--- App/weblogicmvc_pws/webapp/controller/AdminController.php
+++ App/weblogicmvc_pws/webapp/controller/AdminController.php
@@ -4,14 +4,41 @@ use ArmoredCore\Controllers\BaseController;
 use ArmoredCore\Interfaces\ResourceControllerInterface;
 use ArmoredCore\WebObjects\Post;
 use ArmoredCore\WebObjects\Redirect;
+use ArmoredCore\WebObjects\URL;
 use ArmoredCore\WebObjects\View;
 
 class AdminController extends BaseController{
 
 
     public function backoffice(){
-        $users = User::all();
-        View::make('stbox.backoffice', ['users' => $users]);
+        if($_SESSION['admin'] == 1){
+            $users = User::all();
+            return View::make('stbox.backoffice', ['users' => $users]);
+        }else{
+            return View::make('stbox.errorNotLoggedIn');
+        }
     }
 
+    public function blockUser($id){
+
+        $user = User::find($id);
+
+        $user->bloqueado = 1;
+        $user->databloqueado = date("Y/m/d");
+        $user->save();
+
+        $this->backoffice();
+    }
+
+
+    public function unblockUser($id){
+
+        $user = User::find($id);
+
+        $user->bloqueado = 0;
+        $user->databloqueado = null;
+        $user->save();
+
+        $this->backoffice();
+    }
 }
--- App/weblogicmvc_pws/webapp/controller/UserController.php
+++ App/weblogicmvc_pws/webapp/controller/UserController.php
@@ -4,6 +4,7 @@ use ArmoredCore\Controllers\BaseController;
 use ArmoredCore\Interfaces\ResourceControllerInterface;
 use ArmoredCore\WebObjects\Post;
 use ArmoredCore\WebObjects\Redirect;
+use ArmoredCore\WebObjects\URL;
 use ArmoredCore\WebObjects\View;
 
 class UserController extends BaseController implements ResourceControllerInterface
@@ -55,31 +56,45 @@ class UserController extends BaseController implements ResourceControllerInterfa
 
     public function edit($id)
     {
-        $user = User::find($id);
 
 
-        if (is_null($user)) {
-            // redirect to standard error page
-        } else {
-            View::make('stbox.profile', ['user' => $user]);
+        if($_SESSION['id'] == $id){
+            $user = User::find($id);
+
+
+            if (is_null($user)) {
+                // redirect to standard error page
+            } else {
+                View::make('stbox.profile', ['user' => $user]);
+            }
+        }else{
+            //URL::toRoute('user/edit', $_SESSION['id']);
+            Redirect::toRoute('user/edit', $_SESSION['id']);
         }
+
     }
 
     public function update($id)
     {
         $user = User::find($id);
-        $user->update_attributes(Post::getAll());
 
-        if($_POST['password'] != ""){
+
+        /*if($_POST['password'] != ""){
             $user->password = hash('sha1',$_POST['password'], false);
-        }
+    }*/
 
-        if($user->is_valid()){
-            $user->save();
-            Redirect::toRoute('stbox/');
-        } else {
-            // return form with data and errors
-            Redirect::flashToRoute('user/edit', ['user' => $user], $id);
+        if($_POST['password'] == ""){
+            $_SESSION['errorProfile'] = 'É obrigatório colocar password.';
+            Redirect::toRoute('user/edit', $_SESSION['id']);
+        }else{
+            $user->update_attributes(Post::getAll());
+            $user->password = hash('sha1', $_POST['password'], false);
+            if($user->is_valid()){
+                $user->save();
+                Redirect::toRoute('user/edit', $_SESSION['id']);
+            } else {
+                Redirect::flashToRoute('user/edit', ['user' => $user], $id);
+            }
         }
     }
 
@@ -90,33 +105,42 @@ class UserController extends BaseController implements ResourceControllerInterfa
 
     public function login(){
 
+
         $db = mysqli_connect('localhost', 'root', '', 'shuthebox');
 
-        //echo "<script>console.log('Estou no login');</script>";
         $username = $_POST['username'];
         $password =  $_POST['password'];
         $passwordHashed = hash('sha1', $password, false);
 
-        $query = "SELECT id, username, password FROM users WHERE username = '$username' AND password = '$passwordHashed'";
+        $query = "SELECT id, username, password, admin, bloqueado FROM users WHERE username = '$username' AND password = '$passwordHashed'";
 
         $loginResult = mysqli_query($db,$query);
 
         $id = mysqli_fetch_object($loginResult);
 
-        if(mysqli_num_rows($loginResult) == 1){
-            $_SESSION['username'] = $username;
-            $_SESSION['id'] = $id->id;
-            $_SESSION['loggedIn'] = 'Já fez login';
-            Redirect::toRoute('stbox/');
-        }else{
-            /*DEVOLVER PÁGINA DE LOGIN COM O USERNAME INSERIDO NA TEXTBOX E AVISO DE CREDENCIAIS INCORRETAS */
+        if($id->bloqueado == 1){
+            $_SESSION['bloqueado'] = 'Esta conta encontra-se bloqueada';
             Redirect::toRoute('stbox/login');
+        }else{
+            if(mysqli_num_rows($loginResult) == 1){
+                $_SESSION['username'] = $username;
+                $_SESSION['id'] = $id->id;
+                $_SESSION['loggedIn'] = 'Já fez login';
+                $_SESSION['admin'] = $id->admin;
+                Redirect::toRoute('stbox/');
+            }else{
+                $_SESSION['loginErrors'] = 'Credenciais Incorretas';
+                Redirect::toRoute('stbox/login');
+            }
         }
+
     }
 
     public function logOut(){
         session_destroy();
         Redirect::toRoute('stbox/');
     }
+
+
     
 }
