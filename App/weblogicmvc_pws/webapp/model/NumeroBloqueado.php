f03d683c0b02c72cf302d4a0ef18c527a86c7135
David Silvério <2190730@my.ipleiria.pt>
2020-07-08 21:31:22 +0100

    merge entre develop e feature_ds_1

--- App/weblogicmvc_pws/webapp/model/NumeroBloqueado.php
+++ App/weblogicmvc_pws/webapp/model/NumeroBloqueado.php
@@ -14,6 +14,7 @@ class NumeroBloqueado
 
     public function bloquearNumero($numArray, $somaDados) {
         $flag = true;
+        
 
         $somaLocal = array_sum($numArray);
         //$_SESSION['sum'] = $somaLocal;
